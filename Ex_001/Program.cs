b1c94601e568e5a187e450fc60966a64160dc445
DBarrett <darryonbarrett@gmail.com>
2023-06-27 18:48:29 +0300

    Finished work on Ex_001

---
+++ Ex_001/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 34: Задайте массив заполненный случайными 
+// положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int InpitNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(min, max + 1);
+}  
+
+string PrintArray(int[] array)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        res += array[i]  + " ";
+    }
+    return res;
+}
+
+int Find(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+
+
+
+int size = InpitNum("Введите размер массива: ");
+int minValue = InpitNum("Введите минимальное значение элемента: ");
+int maxValue = InpitNum("Введите максимальное значение элемента: ");
+int[] myArray = CreateArray(size);
+FillArray(myArray, minValue, maxValue);
+string txt = PrintArray(myArray);
+Console.WriteLine(txt);
+int count = Find(myArray);
+Console.WriteLine($"Количество четных чисел - {count}");
