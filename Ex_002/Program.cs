1c6ba46047c9b91d2d9434dfc884e5a8887a795c
DBarrett <darryonbarrett@gmail.com>
2023-06-27 19:21:50 +0300

    Finished work on Ex_002

---
+++ Ex_002/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int InpitNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(min, max + 1);
+}  
+
+string PrintArray(int[] array)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        res += array[i]  + " ";
+    }
+    return res;
+}
+
+int SumOdd(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (i % 2 == 1)
+            sum += array[i];
+    return sum;
+}
+
+int size = InpitNum("Введите размер массива: ");
+int minValue = InpitNum("Введите минимальное значение элемента: ");
+int maxValue = InpitNum("Введите максимальное значение элемента: ");
+int[] myArray = CreateArray(size);
+FillArray(myArray, minValue, maxValue);
+string txt = PrintArray(myArray);
+Console.WriteLine(txt);
+Console.WriteLine(SumOdd(myArray));
